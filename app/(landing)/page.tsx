dc5a355d6d88dc4d27be618fe295d74f2eae082a
RobelRH <robel.redda28@gmail.com>
2023-10-30 13:45:45 +0300

    added navbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,13 @@
+import Banner from "@/components/banner";
+import Navbar from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      AI SaaS application Visitors Page
-      <div>
-        <Link href="/sign-in">
-          <Button>
-            Login
-          </Button>
-        </Link>
-        <Link href="/sign-in">
-          <Button>
-            Sign Up
-          </Button>
-        </Link>
-      </div>
+      <Banner />
+      <Navbar />
     </div>
   )
 }
