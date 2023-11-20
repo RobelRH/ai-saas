51271f20f4f80f2b554e2d606f62d999dafb53ea
RobelRH <robel.redda28@gmail.com>
2023-11-20 13:34:59 +0300

    profile issue solved

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,4 +1,4 @@
-import { useUser } from '@clerk/nextjs'
+import { UserButton, useUser } from '@clerk/nextjs'
 import React from 'react'
 import { Avatar, AvatarImage } from './ui/avatar';
 import { AvatarFallback } from '@radix-ui/react-avatar';
@@ -9,11 +9,12 @@ export default function UserAvatar() {
 
     return (
         <Avatar className='h-8 w-8'>
-            <AvatarImage src={user?.profileImageUrl} />
+            <UserButton />
+            {/* <AvatarImage src={user?.profileImageUrl} />
             <AvatarFallback>
                 {user?.firstName?.charAt(0)}
                 {user?.lastName?.charAt(0)}
-            </AvatarFallback>
+            </AvatarFallback> */}
         </Avatar>
     )
 }
