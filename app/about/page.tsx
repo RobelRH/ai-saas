6b1b33039b34636b35f02b5faecd18a294b3e07f
RobelRH <robel.redda28@gmail.com>
2023-11-23 13:19:22 +0300

    update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ function About() {
       <Navbar />
       <div className='px-36 py-12'>
         <div className="px-4 sm:px-0">
-          <h3 className="text-base font-semibold leading-7 text-gray-900">About this website</h3>
+          <h3 className="text-base font-semibold leading-7 text-gray-900">About this website - this is an update</h3>
           <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
         </div>
         <div className="mt-6 border-t border-gray-100">
